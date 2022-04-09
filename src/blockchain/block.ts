c86db97efb0f825130c0c64d83aa562e1aff806f
razbensimon <raz.bensimon@env0.com>
2022-04-09 16:56:17 +0300

    refactor blockchain code to typescript language.
    https://github.com/Savjee/SavjeeCoin/blob/master/src/blockchain.js

---
+++ src/blockchain/block.ts
@@ -0,0 +1,89 @@
+import crypto from "crypto";
+import { Transaction } from "./transaction";
+
+const debug = require("debug")("raz:block");
+
+class Block {
+  private nonce: number;
+  private _hash: string = null!;
+  private _previousHash: string = null!;
+  private readonly _transactions: ReadonlyArray<Transaction> = [];
+
+  constructor(
+    private timestamp: number,
+    transactions: ReadonlyArray<Transaction>,
+    previousHash: string = ""
+  ) {
+    this._transactions = transactions;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+
+  public get hash(): string {
+    return this._hash;
+  }
+
+  private set hash(value: string) {
+    this._hash = value;
+  }
+
+  public get previousHash(): string {
+    return this._previousHash;
+  }
+
+  private set previousHash(value: string) {
+    this._previousHash = value;
+  }
+
+  public get transactions(): ReadonlyArray<Transaction> {
+    return this._transactions;
+  }
+
+  /**
+   * Returns the SHA256 of this block (by processing all the data stored
+   * inside this block)
+   */
+  calculateHash(): string {
+    return crypto
+      .createHash("sha256")
+      .update(
+        this._previousHash +
+          this.timestamp +
+          JSON.stringify(this._transactions) +
+          this.nonce
+      )
+      .digest("hex");
+  }
+
+  /**
+   * Starts the mining process on the block. It changes the 'nonce' until the hash
+   * of the block starts with enough zeros (= difficulty)
+   */
+  mineBlock(difficulty: number): void {
+    while (
+      this._hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this._hash = this.calculateHash();
+    }
+
+    debug(`Block mined: ${this._hash}`);
+  }
+
+  /**
+   * Validates all the transactions inside this block (signature + hash) and
+   * returns true if everything checks out. False if the block is invalid.
+   */
+  hasValidTransactions(): boolean {
+    for (const tx of this._transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+export { Block };
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
-console.log("Hello!");
+import { Transaction, Blockchain } from "./blockchain";
+import { ec as EC } from "elliptic";
+
+const ec = new EC("secp256k1");
+
+// Your private key goes here
+const myKey = ec.keyFromPrivate(
+  "e31fee1f597d6792180e718d10cf000b2586147e124694219898741d04c5daa4"
+);
+
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic("hex");
+
+// Create new instance of Blockchain class
+const razCoin = new Blockchain();
+
+// Mine first block
+razCoin.minePendingTransactions(myWalletAddress);
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, "address2", 100);
+tx1.signTransaction(myKey);
+razCoin.addTransaction(tx1);
+
+// Mine block
+razCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, "address1", 50);
+tx2.signTransaction(myKey);
+razCoin.addTransaction(tx2);
+
+// Mine block
+razCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance is ${razCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// razCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log("Blockchain valid?", razCoin.isChainValid() ? "Yes" : "No");
